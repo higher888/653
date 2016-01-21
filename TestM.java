b92a2aeeefcf50ddb17482e133dd04f75753facd
higher888 <hai_jiang123@outlook.com>
2016-01-21 18:49:04 -0500

    2

--- TestM.java
+++ TestM.java
@@ -8,50 +8,52 @@ import java.io.ByteArrayOutputStream;
 public class TestM {
 
     	/* add your test code here */
-	private M model;
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	@Before public void runBeforeEachTest(){
-		model = new M();
-		System.setOut(new PrintStream(outContent));	 
-	}
-	@After public void runAfterEachTest(){
-		model = null;
-		System.setOut(null);
-	}
-	@Test public void testm_node1(){
-		String s_empty = new String("");
-		model.m(s_empty,0);
-		assertEquals("zero",outContent.toString());
-	}
-	@Test public void testm_node2(){
-		String s_1 = new String("a");
-		model.m(s_1,0);
-		assertEquals("a",outContent.toString());
-	}
-	@Test public void testm_node3(){
-		String s_2 = new String("aa");
-		model.m(s_2,0);
-		assertEquals("b",outContent.toString());	
-	}
-	@Test public void testm_edge1(){
-		String s_3 = new String("");
-		model.m(s_3,0);
-		assertEquals("zero",outContent.toString());
-	}
-	@Test public void testm_edge2(){
-		String s_4 = new String("a");
-		model.m(s_4,1);
-		assertEquals("a",outContent.toString());
-	}
-	@Test public void testm_edge3(){
-		String s_6 = new String("aa");
-		model.m(s_6,1);
-		assertEquals("b",outContent.toString());
-	}
-	@Test public void testm_edge4(){
-		String s_6 = new String("aaa");
-		model.m(s_6,1);
-		assertEquals("b",outContent.toString());
+	public static void main(String [] argv){
+		private M model;
+		private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		@Before public void runBeforeEachTest(){
+			model = new M();
+			System.setOut(new PrintStream(outContent));	 
+		}
+		@After public void runAfterEachTest(){
+			model = null;
+			System.setOut(null);
+		}
+		@Test public void testm_node1(){
+			String s_empty = new String("");
+			model.m(s_empty,0);
+			assertEquals("zero",outContent.toString());
+		}
+		@Test public void testm_node2(){
+			String s_1 = new String("a");
+			model.m(s_1,0);
+			assertEquals("a",outContent.toString());
+		}
+		@Test public void testm_node3(){
+			String s_2 = new String("aa");
+			model.m(s_2,0);
+			assertEquals("b",outContent.toString());	
+		}
+		@Test public void testm_edge1(){
+			String s_3 = new String("");
+			model.m(s_3,0);
+			assertEquals("zero",outContent.toString());
+		}
+		@Test public void testm_edge2(){
+			String s_4 = new String("a");
+			model.m(s_4,1);
+			assertEquals("a",outContent.toString());
+		}
+		@Test public void testm_edge3(){
+			String s_6 = new String("aa");
+			model.m(s_6,1);
+			assertEquals("b",outContent.toString());
+		}
+		@Test public void testm_edge4(){
+			String s_6 = new String("aaa");
+			model.m(s_6,1);
+			assertEquals("b",outContent.toString());
+		}
 	}
 				
     	
